65ed5ab642b9878b4fdd06822122a7103341edcb
N-YASUHIROPC\Yasuhiro Nakano <yookurii@gmail.com>
2018-12-05 14:59:29 +0900

    12/05更新

---
+++ kadai/19/kadai19-1.c
@@ -0,0 +1,18 @@
+/* ‰Û‘è2-1 */
+#include <stdio.h>
+
+int main(void) {
+	int n = 0;
+	char a[100], b[100], c[100], d[100], e[100], f[100], g[100];
+	printf("•¶š—ñ > ");
+	scanf("%s%s%s%s%s%s%s", a, b, c, d, e, f, g);
+	n += strlen(a);
+	n += strlen(b);
+	n += strlen(c);
+	n += strlen(d);
+	n += strlen(e);
+	n += strlen(f);
+	n += strlen(g);
+	printf("7ŒÂ‚Ì•¶š—ñ‚Ì’·‚³‚Ì‡Œv‚Í%d‚Å‚·B\n", n);
+	return 0;
+}
