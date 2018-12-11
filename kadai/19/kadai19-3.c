d6c7c57dd85740c1807160933bd7459e313c5db5
N-YASUHIROPC\Yasuhiro Nakano <yookurii@gmail.com>
2018-12-11 23:34:46 +0900

    12/11更新

--- kadai/19/kadai19-3.c
+++ kadai/19/kadai19-3.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
 int main(void) {
-	int d=0;
+	int n = 0;
 	char moji[100];
 	printf("•¶Žš—ñ > ");
-	scanf("%s",moji);
-	d = 'a' - 'A';
-	for (int i = 0; i <= 100; i++) {
-		if (moji[i] != '\0') {
-			if (moji[i] == 'SPC') {
-				moji[i] = 's';
-			}
-			else if (moji[i] == 's') {
-				moji[i] = 'SPC';
-			}
-			else if (moji[i] == 'n') {
-				moji[i] = '\n';
-			}
-			else {
-				moji[i] += d;
-			}
+	gets(moji);
+	n = strlen(moji);
+	for (int i = 0; i < n; i++) {
+		if (moji[i] == 's') {
+			moji[i] = ' ';
+		}
+		else if (moji[i] == ' ') {
+			moji[i] = 's';
+		}
+			
+		else if (moji[i] == 'n') {
+			moji[i] = '\n';
 		}
 		else {
-			
+			moji[i] = toupper(moji[i]);
 		}
 	}
 	printf("•ÏŠ·Œã‚Ì•¶Žš—ñ > %s\n",moji);
